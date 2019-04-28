24692cb3f9578a21e378c9d05be23f5993fec073
Jeff is trying to learn more about programming <notmyemail@skld.com>
2019-04-28 16:47:16 +0000

    modified config/environments dev and production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,7 +1,17 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-
+  config.assets.initialize_on_precompile = false
   # Code is not reloaded between requests.
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :port           => ENV['MAILGUN_SMTP_PORT'],
+    :address        => ENV['MAILGUN_SMTP_SERVER'],
+    :user_name      => ENV['MAILGUN_SMTP_LOGIN'],
+    :password       => ENV['MAILGUN_SMTP_PASSWORD'],
+    :domain         => 'myapp.herokuapp.com', #mydomain actually contains the realvalue
+    :authentication => :plain,
+  }
   config.cache_classes = true
 
   # Eager load code on boot. This eager loads most of Rails and
